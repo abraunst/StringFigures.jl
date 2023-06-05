899e66ca9b2c6e92689a614d21d1356c370c2915
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-06-05 07:10:05 +0200

    move functions to functions.jl

--- src/StringFigures.jl
+++ src/StringFigures.jl
@@ -2,6 +2,7 @@ module StringFigures
 
 include("linearsequence.jl")
 include("embedding.jl")
+include("functions.jl")
 include("functors.jl")
 
 export LinearSequence, SeqNode, depth, @seq_str, @node_str, @f_str, plot,
