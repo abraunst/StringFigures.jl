466c0f6813eead76f83e5a3268c76b12c8b17ea8
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-06-07 09:15:26 +0200

    first work on heart sequences

--- src/StringFigures.jl
+++ src/StringFigures.jl
@@ -5,7 +5,7 @@ include("embedding.jl")
 include("functions.jl")
 include("functors.jl")
 
-export LinearSequence, SeqNode, depth, @seq_str, @node_str, @f_str, plot,
+export LinearSequence, SeqNode, depth, @seq_str, @node_str, @heart_str, plot,
         isframenode, isfarsidenext, isnearsidenext, iscanonical,
         canonical, release, simplify, pick
 end
--- src/functors.jl
+++ src/functors.jl
@@ -2,6 +2,8 @@ abstract type Functor end
 
 struct ExtendFunctor <: Functor end
 
+string(f::ExtendFunctor) = "|"
+
 (f::ExtendFunctor)(p::LinearSequence) = simplify(p)
 
 struct PickFunctor <: Functor
@@ -11,16 +13,42 @@ struct PickFunctor <: Functor
     over::Bool
 end
 
+string(f::PickFunctor) = "$(string(f.fun))$(f.over ? "o" : "u")($(string(f.arg))$(f.near ? "n" : "f"))"
+
 (f::PickFunctor)(p::LinearSequence) = pick(p, f.over, f.fun, f.arg, f.near)
 
 struct ReleaseFunctor <: Functor
     arg::SeqNode
 end
 
+string(f::ReleaseFunctor) = "□$(string(f.arg))"
+
 (f::ReleaseFunctor)(p::LinearSequence) = release(p, f.arg)
 
 
-macro f_str(s)
+struct HeartSequence 
+    seq::Vector{Functor}
+end
+
+Base.:(*)(s::HeartSequence, t::HeartSequence) = HeartSequence(vcat(s.seq,t.seq))
+
+Base.:(*)(s::HeartSequence, t::Functor) = HeartSequence(vcat(s.seq,[t]))
+
+Base.:(*)(s::Functor, t::HeartSequence) = HeartSequence(vcat([s],t.seq))
+
+Base.:(*)(s::Functor, t::Functor) = HeartSequence([s,t])
+
+Base.:(^)(s::Functor, k::Integer) = HeartSequence(fill(s,k))
+
+Base.:(^)(s::HeartSequence, k::Integer) = HeartSequence(reduce(vcat, (s.seq for i=1:k)))
+
+(s::HeartSequence)(p::LinearSequence) = ∘(s.seq...)(p)
+
+Base.show(io::IO, s::HeartSequence) = print(io, "heart\"", join(string.(s.seq), ":"), "\"")
+
+
+
+function string2functor(s)
     m = match(r"\|", s)
     !isnothing(m) && return ExtendFunctor()
     m = match(r"□([LR]\d+)", s) 
@@ -28,4 +56,8 @@ macro f_str(s)
     m = match(r"([LR]\d+)([ou])\(([LR]\d+)([nf])\)", s) 
     !isnothing(m) && return PickFunctor(SeqNode(m[1]),SeqNode(m[3]), m[4] == "n", m[2] == "o")
     throw(ArgumentError("Could not parse $s"))
+end
+
+macro heart_str(s)
+    HeartSequence(string2functor.(split(s,":")))
 end
