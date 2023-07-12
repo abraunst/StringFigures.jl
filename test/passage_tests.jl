f5486b2ed4022e078a92996ef20e9cd207738bfc
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-07-12 21:00:49 +0200

    more calc tests

--- test/passage_tests.jl
+++ test/passage_tests.jl
@@ -45,4 +45,7 @@ end
 @testset "calc" begin
     C = calc"R2o(L1f)#L2u(R5n)"
     @test C(O1) == OA
+    O1b = pass"R2o(L1f)"(O1)
+    @test O1b == seq"L1:x1(0):R2:x2(0):L5:R5:x2(U):x1(U):R1"
+    @test pass"L2u(R5n)"(O1b) == seq"L1:x1(0):R2:x2(0):L5:R5:x2(U):L2:x1(U):R1"
 end
