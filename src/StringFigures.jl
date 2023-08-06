37bfcfe9aefc9017eb1ca8270c933466eb1c0c8a
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-08-06 10:56:10 +0200

    add missing exports

--- src/StringFigures.jl
+++ src/StringFigures.jl
@@ -8,9 +8,12 @@ include("passages.jl")
 include("calculus.jl")
 
 export LinearSequence, SeqNode, FrameNode, CrossNode,
-        @node_str, @seq_str, @storer_str, @pass_str, @calc_str, @proc_str, plot, latex,
-        isframenode, isfarsidenext, isnearsidenext, iscanonical,
-        canonical, release, simplify, pick, twist,
-        Passage, ExtendPassage, PickPassage, ReleasePassage, TwistPassage, 
+        @node_str, @seq_str, @storer_str, @pass_str, @calc_str, @proc_str, 
+        plot, latex, isframenode, isfarsidenext, isnearsidenext,
+        iscanonical, canonical, release, simplify, pick, twist,
+        Passage, ExtendPassage, PickPassage, ReleasePassage, 
+        TwistPassage, NavahoPassage,
+        BilateralPickPassage, BilateralReleasePassage, 
+        BilateralTwistPassage, BilateralNavahoPassage,
         StringCalculus, StringProcedure
 end
