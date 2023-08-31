9cdb7585ac953bcc1ec7d075f4587cd4a24e0477
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-08-31 19:36:53 +0200

    - fix bug in pick in which the above param was ignored, by yulong - fix bug in pick in which the direction of the pick was wrong

--- src/functions.jl
+++ src/functions.jl
@@ -166,18 +166,21 @@ end
 
 function pick(p::LinearSequence, f::FrameNode, args::Vector{Tuple{FrameNode, Bool, Bool}}, above::Bool=false)
     arg, _, over = args[end]
-    type(f) == type(arg) && return pick_sameside(p, f, args)
-    extra = idx(f) > idx(arg) ? (0,0) : (6,0) ## check
-    farg, ffun = FrameNode(type(arg), extra...), FrameNode(type(f), extra...)
-    p = pick_sameside(p, farg, args)
-    p.seq[findframenode(farg, p)] = ffun
-    #println(p)
-    p = pick_sameside(p, over, f, ffun, idx(f) < extra)
-    p = release(p, ffun) 
+    if type(f) == type(arg) 
+        p = pick_sameside(p, f, args)
+    else
+        extra = idx(f) < idx(arg) ? (0,0) : (6,0) ## check
+        farg, ffun = FrameNode(type(arg), extra...), FrameNode(type(f), extra...)
+        p = pick_sameside(p, farg, args)
+        p.seq[findframenode(farg, p)] = ffun
+        #println(p)
+        p = pick_sameside(p, over, f, ffun, idx(f) < extra)
+        p = release(p, ffun)
+    end
     if above
         p = twist(p, f, idx(f) < idx(arg))
     end
-    p |> simplify
+    simplify(p)
 end
 
 function twist(p::LinearSequence, f::FrameNode, away::Bool)
