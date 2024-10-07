b8043eaf04200cd431c03f7e35d4abbccfbac99d
Alfredo Braunstein <alfredo.braunstein@polito.it>
2024-10-07 13:30:41 +0200

    fat fingers seem to work

--- src/embedding.jl
+++ src/embedding.jl
@@ -16,68 +16,6 @@ function tutte_embedding(g; vfixed=1:0, locs_fixed=fill(0.0, 0, 2))
 end
 
 
-# Copied and modified from GraphLayout.jl
-function spring_layout_fixed(g::AbstractGraph;
-        locs_x = 2 .* rand(nv(g)) .- 1.0,
-        locs_y = 2 .* rand(nv(g)) .- 1.0,
-        vfixed = 1:0,
-        C = 2.0,
-        k = C * sqrt(4.0 / nv(g)),
-        MAXITER = 100,
-        INITTEMP = 2.0)
-
-    adj_matrix = adjacency_matrix(g)
-
-    # The optimal distance bewteen vertices
-    k² = k^2
-
-    # Store forces and apply at end of iteration all at once
-    force_x = zeros(nv(g))
-    force_y = zeros(nv(g))
-
-    # Iterate MAXITER times
-    for iter = 1:MAXITER
-    # Calculate forces
-        for i = 1:nv(g)
-            force_vec_x = 0.0
-            force_vec_y = 0.0
-            for j = 1:nv(g)
-                i == j && continue
-                d_x = locs_x[j] - locs_x[i]
-                d_y = locs_y[j] - locs_y[i]
-                dist²  = d_x^2 + d_y^2
-                dist = sqrt(dist²)
-
-                if !(iszero(adj_matrix[i,j]) && iszero(adj_matrix[j,i]))
-                    # Attractive + repulsive force
-                    # F_d = dist² / k - k² / dist # original FR algorithm
-                    F_d = dist / k - k² / dist²
-                else
-                    # Just repulsive
-                    # F_d = -k² / dist  # original FR algorithm
-                    F_d = -k² / dist²
-                end
-                force_vec_x += F_d*d_x
-                force_vec_y += F_d*d_y
-            end
-            force_x[i] = force_vec_x
-            force_y[i] = force_vec_y
-        end
-        # Cool down
-        temp = INITTEMP / iter
-        # Now apply them, but limit to temperature
-        for i in 1:nv(g)
-            i ∈ vfixed && continue
-            force_mag  = sqrt(force_x[i]^2 + force_y[i]^2)
-            scale      = min(force_mag, temp) / force_mag
-            locs_x[i] += force_x[i] * scale
-            locs_y[i] += force_y[i] * scale
-        end
-    end
-    return locs_x, locs_y
-end
-
-
 function node_labels_and_fixed_positions(p::LinearSequence; crossings=true)
     D = Dict{SeqNode, Int}()
     function pos(n)
@@ -176,7 +114,6 @@ end
 
 Plots `p` using Tutte embedding. Parallel edges are then separated by slightly translating them perpendicularly to the segment joining the two vertices.
 
-* `k::Float64`:        Set `k>0` to relayout using a very small repulsive force (`0.0`).
 * `fact::Float64`:     Multiplicative factor for edge width
 * `rfact::Float64`:    Distance between parallel edges (`0.02`)
 * `randomize::Bool`:   Slightly randomize positions (`false`) 
@@ -189,7 +126,7 @@ Plots `p` using Tutte embedding. Parallel edges are then separated by slightly t
 Example1: plot(proc"OA::DL1|")
 Example2: plot(proc"OA::DL1|"; shadowc="white", stringc="black", nodelabelc="black", background_color="white")
 """
-function plot(p::LinearSequence; rfact=0.02, k=0.0, randomize=false, crossings=false,
+function plot(p::LinearSequence; rfact=0.02, randomize=false, crossings=false,
             labels=true, shadowc = HSLA(colorant"black", 1.0), stringc=colorant"white", fact=1.0, 
             nodelabelc=colorant"white", kwd...)
     
@@ -227,6 +164,7 @@ function plot(p::LinearSequence; rfact=0.02, k=0.0, randomize=false, crossings=f
 
     g = SimpleGraphFromIterator(Iterators.flatten((underlist, overlist)))
 
+
     append!(vlabels, fill("", nv(g)-n))
 
     locs_x, locs_y = tutte_embedding(g; vfixed, locs_fixed);
@@ -236,7 +174,6 @@ function plot(p::LinearSequence; rfact=0.02, k=0.0, randomize=false, crossings=f
     end
     
     for i in eachindex(p)
-        #(isframenode(p[i+1]) || isframenode(p[i-1])) && continue
         P1 = SVector(locs_x[index(p[i+1])], locs_y[index(p[i+1])])
         P2 = SVector(locs_x[index(p[i+2])], locs_y[index(p[i+2])])
         P3 = SVector(locs_x[index(p[i  ])], locs_y[index(p[i  ])])
@@ -246,15 +183,10 @@ function plot(p::LinearSequence; rfact=0.02, k=0.0, randomize=false, crossings=f
         P34 = iszero(P3-P4) ? SVector(0.0,0.0) : (P3-P4)/norm(P3-P4)
         P13 = iszero(P1-P3) ? SVector(0.0,0.0) : (P1-P3)/norm(P1-P3)
         D = P12 + P34 - ((P12 + P34) ⋅ P13) * P13
-        locs_x[n + i] += D[1] * rfact 
-        locs_y[n + i] += D[2] * rfact
+        locs_x[n + i] += D[1] * (isframenode(p[i]) ? rfact/5 : rfact) 
+        locs_y[n + i] += D[2] * (isframenode(p[i]) ? rfact/5 : rfact)
     end 
 
-    if k > 0
-        # find spring embedding with small repulsive forces
-        locs_x, locs_y = spring_layout_fixed(g; vfixed, locs_x, locs_y, k)
-    end
-    
     pl0 = gplot(g, locs_x, locs_y;
         NODELABELSIZE=0.0, NODESIZE=0.0, EDGELINEWIDTH=1.0 * fact, edgestrokec=shadowc, kwd...)
 
@@ -266,12 +198,29 @@ function plot(p::LinearSequence; rfact=0.02, k=0.0, randomize=false, crossings=f
 
     pl3 = gplot(gover, locs_x, locs_y;
         EDGELINEWIDTH=0.2*fact, edgestrokec=stringc,
-        NODESIZE=[i ∈ vfixed ? 0.008 : (crossings ? 0.002 : 0.0) for i in 1:nv(g)],
+        NODESIZE=[i ∈ vfixed ? 0.00 : (crossings ? 0.002 : 0.0) for i in 1:nv(g)],
         nodefillc=[i ∈ vfixed ? colorant"red" : stringc for i in 1:nv(g)],
-        NODELABELSIZE=2.0, nodelabel=labels ? @view(vlabels[1:nv(gover)]) : nothing, nodelabeldist=9, 
+        NODELABELSIZE=0.0, nodelabel=nothing, nodelabeldist=9, 
         nodelabelc
         )
+    
+    oldl = length(locs_x)
+    for i=1:2:lastindex(vfixed)
+        push!(locs_x, (locs_x[vfixed[i]]+locs_x[vfixed[i+1]])/2)
+        push!(locs_y, (locs_y[vfixed[i]]+locs_y[vfixed[i+1]])/2)
+        push!(vlabels, vlabels[vfixed[i]]*vlabels[vfixed[i+1]])
+        add_vertex!(g)
+    end
 
-    compose(pl3,pl2,pl1,pl0)
+    pl4 = gplot(g, locs_x, locs_y; 
+        NODESIZE=[(i ≤ oldl ? 0.0 : 0.008) for i=1:length(locs_x)], 
+        nodelabel=labels ? [(i ≤ oldl ? "" : vlabels[i]) for i=1:nv(g)] : nothing,
+        EDGELINEWIDTH=0,
+        nodefillc=colorant"red",
+        nodelabeldist=9,
+        nodelabelc,
+        NODELABELSIZE=2.0)
+
+    compose(pl4,pl3,pl2,pl1,pl0)
     #compose(pl2)
 end
