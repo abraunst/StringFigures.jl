2621f711a1ae6d0c795e0fccad0d35a0133cd7fe
Alfredo Braunstein <alfredo.braunstein@polito.it>
2024-10-22 08:55:28 +0200

    whitespace

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,22 @@
 using Test, Documenter, StringFigures
 
-    @testset "canonical" begin
-        include("canonical_tests.jl")
-    end
+@testset "canonical" begin
+    include("canonical_tests.jl")
+end
 
-    @testset "parse" begin
-        include("parse_tests.jl")
-    end
+@testset "parse" begin
+    include("parse_tests.jl")
+end
 
-    @testset "passage" begin
-        include("passage_tests.jl")
-    end
+@testset "passage" begin
+    include("passage_tests.jl")
+end
 
-    @testset "calculus" begin
-        include("calculus_tests.jl")
-    end
+@testset "calculus" begin
+    include("calculus_tests.jl")
+end
 
-    @testset "doctests" begin
-        DocMeta.setdocmeta!(StringFigures, :DocTestSetup, :(using StringFigures); recursive=true)
-        doctest(StringFigures; manual = false)
-    end
+@testset "doctests" begin
+    DocMeta.setdocmeta!(StringFigures, :DocTestSetup, :(using StringFigures); recursive=true)
+    doctest(StringFigures; manual = false)
+end
